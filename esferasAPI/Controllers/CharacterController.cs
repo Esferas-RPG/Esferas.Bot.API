1a86626b1ed14c9cf3be22bbdb8e82deda0efe23
Luanderson Mendes <contato@luanderson.dev.br>
2024-11-09 10:31:26 -0400

    revertido branch do alex, pq fiz cagada

--- esferasAPI/Controllers/CharacterController.cs
+++ esferasAPI/Controllers/CharacterController.cs
@@ -45,8 +45,7 @@ namespace apiEsferas.Controllers
                 return StatusCode(500, $"Error:{ex.Message}");
             }
         }
-
-        //* listar players
+    
         [HttpGet("listPlayers")]
         public async Task<IActionResult> ListPlayers()
         {
@@ -57,17 +56,13 @@ namespace apiEsferas.Controllers
             });
         }
 
+        [HttpPost("moveFile")]
+        public async Task<IActionResult> moveFiles([FromBody] moveFilesRequest request)
+        {
+            var fileLink = request.fileLink;
+            var destinationFolderId = request.destinationLink;
+        }
 
-        //* Não mexer nesta caralha - to fazendo ainda luan
-        // [HttpPost("moveFile")]
-        // public async Task<IActionResult> moveFiles([FromBody] moveFilesRequest request)
-        // {
-        //     var fileLink = request.fileLink;
-        //     var destinationFolderId = request.destinationLink;
-        // }
-
-
-        //* delete the log
         [HttpPost("deleteSheets")]
         public async Task<IActionResult> deleteCharacter([FromBody] CharacterDeleteRequest request)
         {
